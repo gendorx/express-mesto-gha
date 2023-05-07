ca29f5a0e28f45fca5c5ab7ac29df7a17c11e0de
Andrew <cb.project@ya.ru>
2023-05-07 09:34:51 +0000

    added: not found to not exists links

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { NotFound } = require('../utils/errors');
 
 const router = express.Router();
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+
+router.all('*', (req, res, next) => {
+  next(NotFound('неверный адрес запроса'));
+});
 
 module.exports = router;
